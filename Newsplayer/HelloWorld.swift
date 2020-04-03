e3b33b60d21e374ae638f6b7482e6b749a5253b2
Banchai <>
2020-04-03 21:18:43 +0700

    Initial commit

---
+++ Newsplayer/HelloWorld.swift
@@ -0,0 +1,16 @@
+//
+//  HelloWorld.swift
+//  Newsplayer
+//
+//  Created by Banchai on 3/4/2563 BE.
+//  Copyright © 2563 Banchai. All rights reserved.
+//
+
+import UIKit
+
+class HelloWorld: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor  = .systemRed
+    }
+}
---
+++ Newsplayer/Newsplayer.h
@@ -0,0 +1,19 @@
+//
+//  Newsplayer.h
+//  Newsplayer
+//
+//  Created by Banchai on 3/4/2563 BE.
+//  Copyright © 2563 Banchai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Newsplayer.
+FOUNDATION_EXPORT double NewsplayerVersionNumber;
+
+//! Project version string for Newsplayer.
+FOUNDATION_EXPORT const unsigned char NewsplayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Newsplayer/PublicHeader.h>
+
+
